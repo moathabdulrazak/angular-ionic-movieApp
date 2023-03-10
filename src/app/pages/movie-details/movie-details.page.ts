18ea686202e8c8458b25064aa2665c76258978a1
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-10 02:48:38 -0700

    movie details page

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movie-details',
@@ -11,7 +12,7 @@ export class MovieDetailsPage implements OnInit {
 
   constructor(private route: ActivatedRoute, private movieService: MovieService) { }
   movie: any;
-
+imageBaseUrl = environment.images;
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id')!.toString();
     this.movieService.getDetails(id).subscribe((res) => {
